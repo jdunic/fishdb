cb81678d305822c73f37c79a8dadc13459d786a8
J Dunic <jdunic+github@gmail.com>
2013-01-31 00:13:54 -0800

    added a note

--- fishdb/models.py
+++ fishdb/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+# @TODO: Jillian to complete
+
 class Sites(models.Model):
     Latitude = models.CharField(max_length=255)
     Longitude = models.CharField(max_length=255)
